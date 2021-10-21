5f8f4b584fbecad0e586634fbc8601f18bc7ee4d
ariasmiyagi <arias-miyagi-20f@stu.kbc.ac.jp>
2021-10-21 15:04:44 +0900

    １

---
+++ 25miyagi/competition/Assets/Memo.cs
@@ -0,0 +1,7 @@
+﻿//主グリッド 1m * 1m
+//副グリッド 10cm * 10cm
+//Cubeのサイズ	10cm*10cm
+//Sphereのサイズ	直径10cm
+//Plane	1m*1m
+//Unity内の距離1.0f	10cm
+//UnityのRigidbodyのMass 1.0f	1.0kg
---
+++ 25miyagi/competition/Assets/scripts/Tilt.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour
+{
+    Transform Tx, Tz;
+    float Ax, Az;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tx = GetComponent<Transform>();
+        Tz = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ax = Tx.localEulerAngles.x; //W,S
+        Az = Tz.localEulerAngles.z; //A,D
+
+        Debug.Log(Ax + "," + Az);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //if(Az <= 30f)
+            //{
+                transform.Rotate(0, 0, 1);
+            //}
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //if (Az <= 330)
+            //{
+                transform.Rotate(0, 0, -1);
+            //}
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Ax <= 30f)
+            {
+                transform.Rotate(1, 0, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Ax >= -30f)
+            {
+                transform.Rotate(-1, 0, 0);
+            }
+        }
+    }
+}
