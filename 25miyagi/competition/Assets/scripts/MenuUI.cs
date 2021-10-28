331f9eb378e6246c81b4a43a6c9fdc7aebc2875c
KBC12005016\kbc12005016 <yuya-uchihara-20f@stu.kbc.ac.jp>
2021-10-28 11:27:19 +0900

    更新

--- 25miyagi/competition/Assets/scripts/MenuUI.cs
+++ 25miyagi/competition/Assets/scripts/MenuUI.cs
@@ -3,12 +3,12 @@ using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
-    
+
     [SerializeField] private GameObject pausePanel;
     int startflg = 0;
     int Pausecount = 0;
-    
-  
+
+
     void Start()
     {
         pausePanel.SetActive(false);
@@ -16,22 +16,19 @@ public class MenuUI : MonoBehaviour
 
     void Update()
     {
-        bool start = Input.GetKeyDown("joystick button 9");
+        bool start = Input.GetKeyDown("joystick button 7");
 
-        if (start == true && Pausecount == 0) 
+        if (start == true && Pausecount == 0)
         {
             Time.timeScale = 0;
             pausePanel.SetActive(true);
             Pausecount = 1;
-
-        }else if (start == true && Pausecount == 1){
-            Time.timeScale = 0;
+        }
+        else if (start == true && Pausecount == 1)
+        {
+            Time.timeScale = 1;
             pausePanel.SetActive(false);
             Pausecount = 0;
-
         }
-
     }
-
-   
 }
