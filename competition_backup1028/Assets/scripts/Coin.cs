559da379f56b253362822f5a60bbd8d9d01236d8
KBC12005016\kbc12005016 <yuya-uchihara-20f@stu.kbc.ac.jp>
2021-11-04 10:05:24 +0900

    いろいろアプデ

--- competition_backup1028/Assets/scripts/Coin.cs
+++ competition_backup1028/Assets/scripts/Coin.cs
@@ -2,32 +2,39 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Coin : MonoBehaviour
 {
-    public GameObject unlockObj;
+    //public GameObject unlockObj;
     public TextMeshProUGUI countText;
 
     private int count;
 
+
     void Start()
     {
+
         count = 0;
         //countText.text = "0/12" + count.ToString();
-        countText.text = count.ToString() + "/12";
+        //countText.text = count.ToString() + "/12";
+        Score();
     }
 
-    private void OnCollisionEnter(Collision other)
+    public void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "Ball")
+        if (other.gameObject.CompareTag("Coin"))
         {
             //unlockObj.gameObject.SetActive(false);
-            this.gameObject.SetActive(false);
-            count++;
-            //countText.text = "0/12" + count.ToString();
-            countText.text = count.ToString() + "/12";
-
+            other.gameObject.SetActive(false);
+             count += 1;
+            //countText.text =  count.ToString() + "/12";
+            Score();
         }
+    }
+    void Score()
+    {
+        countText.text = count.ToString() + "/12";
     }
 }
