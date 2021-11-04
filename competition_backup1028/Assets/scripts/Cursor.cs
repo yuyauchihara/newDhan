60d843285f89004614274501b103c4557ea83397
KBC12005016\kbc12005016 <yuya-uchihara-20f@stu.kbc.ac.jp>
2021-11-04 15:18:34 +0900

    no message

---
+++ competition_backup1028/Assets/scripts/Cursor.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Cursor : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Cursor1;
+    [SerializeField] private GameObject Cursor2;
+    [SerializeField] private GameObject Cursor3;
+    int startflg = 0;
+    int Cursorcount = 1;
+    float x;
+
+    float af = 0;
+
+
+
+
+    void Start()
+    {
+        Cursor2.SetActive(false);
+        Cursor3.SetActive(false);
+
+    }
+
+    void Update()
+    {
+
+        ////////////////
+        ///カーソルの位置を変えるコード
+
+        x = Input.GetAxisRaw("Vertical");
+
+
+        if (Cursorcount == 1 && x == 1)
+        {
+            Cursorcount = 3;
+            Cursor1.SetActive(false);
+            Cursor3.SetActive(true);
+        }
+        else if (Cursorcount == 1 && x == -1)
+        {
+            Cursorcount = 2;
+            Cursor1.SetActive(false);
+            Cursor2.SetActive(true);
+        }
+
+        //カーソルが２にある時の操作
+        else if (Cursorcount == 2 && x == 1)
+        {
+            Cursorcount = 1;
+            Cursor2.SetActive(false);
+            Cursor1.SetActive(true);
+
+            if (Cursorcount == 1)
+            {
+
+            }
+
+        }
+        else if (Cursorcount == 2 && x == -1)
+        {
+            Cursorcount = 3;
+            Cursor2.SetActive(false);
+            Cursor3.SetActive(true);
+
+            if (Cursorcount == 3)
+            {
+
+            }
+        }
+
+        //カーソルが３にある時の操作
+        else if (Cursorcount == 3 && x == 1)
+        {
+            Cursorcount = 2;
+            Cursor3.SetActive(false);
+            Cursor2.SetActive(true);
+
+            if (Cursorcount == 2)
+            {
+
+            }
+        }
+        else if (Cursorcount == 3 && x == -1)
+        {
+            Cursorcount = 1;
+            Cursor3.SetActive(false);
+            Cursor1.SetActive(true);
+
+
+            if (Cursorcount == 1)
+            {
+
+            }
+
+        }
+
+
+        //カーソルが１にある時の操作
+
+
+        ///カーソルの位置を変えるコード終わり
+        ////////////////
+
+
+        ////////////////
+        ///カーソルの指して項目を選択した時に実行するコード
+
+
+        bool kettei = Input.GetKeyDown("joystick button 0");
+        if (kettei == true && Cursorcount == 1)
+        {
+
+
+        }
+        else if (kettei == true && Cursorcount == 2)
+        {
+
+
+        }
+        else if (kettei == true && Cursorcount == 3)
+        {
+            Application.Quit();
+
+        }
+
+
+        ///カーソルの指して項目を選択した時に実行するコード終わり
+        ////////////////
+
+
+    }
+}
