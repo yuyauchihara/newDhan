64e641da70002609d5eebe509df4acb50be95fe0
KBC12005016\kbc12005016 <yuya-uchihara-20f@stu.kbc.ac.jp>
2021-11-11 12:54:07 +0900

    エフェクトとSE

---
+++ competition_backup1108/Assets/Scenes/scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject particleObject;
+    GameObject parclon;
+    void Start()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            parclon = Instantiate(particleObject, this.transform.position, Quaternion.identity);
+            Destroy(parclon, 1.0f);
+        }
+    }
+}
