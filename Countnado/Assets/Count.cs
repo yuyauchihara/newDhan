1e68e8c1ebbd56005326ee0146c815e101d4cb8c
KBC11803045\tobita satoki <tobita-satoki-19c@stu.kbc.ac.jp>
2021-11-17 11:53:30 +0900

    no message

---
+++ Countnado/Assets/Count.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+	[SerializeField]
+	private Text _textCountdown;
+
+	[SerializeField]
+	private Image _imageMask;
+
+	void Start()
+	{
+		_textCountdown.text = "";
+	}
+
+	public void OnClickTextStart()
+	{
+		StartCoroutine(CountdownCoroutine());
+	}
+
+	IEnumerator CountdownCoroutine()
+	{
+		_imageMask.gameObject.SetActive(true);
+		_textCountdown.gameObject.SetActive(true);
+
+		_textCountdown.text = "3";
+		yield return new WaitForSeconds(60.0f);
+
+		_textCountdown.text = "2";
+		yield return new WaitForSeconds(60.0f);
+
+		_textCountdown.text = "1";
+		yield return new WaitForSeconds(60.0f);
+
+		_textCountdown.text = "スターと!";
+		yield return new WaitForSeconds(30.0f);
+
+		_textCountdown.text = "";
+		_textCountdown.gameObject.SetActive(false);
+		_imageMask.gameObject.SetActive(false);
+	}
+}
---
+++ competition_backup1108/Assets/scripts/Count.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Count : MonoBehaviour
+{
+    public Text countDownText;
+    [SerializeField] GameObject panel;
+    private void Start()
+    {
+        Time.timeScale = 0f;
+        StartCoroutine("CountDown");
+    }
+
+    IEnumerator CountDown()
+    {
+        for (int i = 2; i >= 0; i--)
+        {
+            yield return new WaitForSecondsRealtime(1);
+            countDownText.text = i.ToString();
+            if(i == 0)
+            {
+                countDownText.text = "スターと".ToString();
+            }
+        }
+        yield return new WaitForSecondsRealtime(0.5f);
+        Time.timeScale = 1f;
+    }
+}
